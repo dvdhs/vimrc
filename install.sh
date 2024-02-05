bf10e6217beafa1cec8ef8f486ce2901694209ff
David Suh <davidsuh@Davids-MacBook-Pro.local>
2024-02-04 21:13:24 -0600

    done

---
+++ install.sh
@@ -0,0 +1,12 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors
+curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+nvm install 15.0.0
+node -e "console.log('Running Node.js ' + process.version)"
