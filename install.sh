347bf0bf197a9425bff0ffd0da44deab498561fe
David Suh <davidsuh@Davids-MacBook-Pro.local>
2024-02-04 21:52:38 -0600

    innstall

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+curl -o ~/.vimrc https://github.com/davidsuh11/vimrc/blob/main/.vimrc
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
